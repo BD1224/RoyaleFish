0b07e6c69f9c8c319c5c38d38161f4c6e0b88949
BD1224 <benjaminduboshinsky@gmail.com>
2026-02-21 11:10:34 -0500

    Initial Commit

---
+++ RoyaleFish/RoyaleFishApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoyaleFishApp.swift
+//  RoyaleFish
+//
+//  Created by Benjamin Duboshinsky on 2/21/26.
+//
+
+import SwiftUI
+
+@main
+struct RoyaleFishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
